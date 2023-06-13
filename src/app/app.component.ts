99561d98e828c650a64012cabe7b156eea11794c
Patel <ppatel81@dxc.com>
2023-06-13 12:57:22 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testApp';
+  constructor(private http: HttpClient) { }
+  onClickSubmit(data: any) {
+    this.http.post<any>('https://formspree.io/f/xayzrngw', data).subscribe(data => {
+      console.log(data)
+  });
+ }
 }
